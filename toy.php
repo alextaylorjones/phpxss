14cd7b5a3d36b9fffd8528c8c4361669d0e43280
alextaylorjones <alexjone@usc.edu>
2017-12-04 19:44:18 -0800

    Update toy.php

--- toy.php
+++ toy.php
@@ -1,1 +1,14 @@
-<!-- #exec cmd="ls" -->
+<?php
+
+echo "hello world";
+$browser="HO";
+
+return $browser;
+
+?>
+
+<?php 
+
+include('https://raw.githubusercontent.com/alextaylorjones/phpxss/master/toy.php');
+
+?>
